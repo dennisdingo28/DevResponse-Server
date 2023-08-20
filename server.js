2c8f769ede29bdbca82257a7d5399866deac0770
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-08-20 21:39:43 +0300

    server socket

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const http = require("http");
+const express = require('express');
+const {Server} = require("socket.io");
+
+const helmet = require('helmet');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+    } 
+});
+
+
+io.on("connection",(socket)=>{
+    console.log(socket.id);
+    socket.on("new_bug",msg=>{
+        console.log("new bug post",msg);
+    })
+});
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors()); 
+
+app.get('/',(req,res)=>{
+    res.status(200).send('This is the server')
+})
+
+server.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT}`);
+});
